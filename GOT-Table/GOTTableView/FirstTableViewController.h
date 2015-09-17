232d27c3dd8b6c8d58a278bc33d47f909d231838
matteosamaranch <matteosamaranch@gmail.com>
2015-09-17 09:50:01 +0200

    Created first TableViewController

---
+++ GOT-Table/GOTTableView/FirstTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  FirstTableViewController.h
+//  GOTTableView
+//
+//  Created by Mateo Mauricio Samaranch on 17/09/15.
+//  Copyright (c) 2015 Ricardo Sanchez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FirstTableViewController : UITableViewController
+
+@end
