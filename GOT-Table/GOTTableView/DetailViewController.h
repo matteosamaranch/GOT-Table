014315a79977de978f57a83827708f25ffdfbba7
matteosamaranch <matteosamaranch@gmail.com>
2015-09-17 12:37:59 +0200

    9ºCommit
    Properties y outlets

---
+++ GOT-Table/GOTTableView/DetailViewController.h
@@ -0,0 +1,17 @@
+//
+//  DetailViewController.h
+//  GOTTableView
+//
+//  Created by Mateo Mauricio Samaranch on 17/09/15.
+//  Copyright (c) 2015 Ricardo Sanchez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "Personaje.h"
+
+@interface DetailViewController : UIViewController
+
+@property (strong, nonatomic) Personaje *personaje;
+
+@end
---
+++ GOT-Table/GOTTableView/DetailViewController.m
@@ -0,0 +1,49 @@
+//
+//  DetailViewController.m
+//  GOTTableView
+//
+//  Created by Mateo Mauricio Samaranch on 17/09/15.
+//  Copyright (c) 2015 Ricardo Sanchez. All rights reserved.
+//
+
+#import "DetailViewController.h"
+
+@interface DetailViewController ()
+
+@property (weak, nonatomic) IBOutlet UIImageView *nameImage;
+@property (weak, nonatomic) IBOutlet UITextView *biographyTextview;
+@property (weak, nonatomic) IBOutlet UILabel *nameLabel;
+
+@end
+
+@implementation DetailViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)viewWillAppear:(BOOL)animated
+{
+    self.nameImage.image = [UIImage imageNamed:self.personaje.imagen];
+    self.nameLabel.text = self.personaje.nombre;
+    self.biographyTextview.text = self.personaje.descripcion;
+
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
